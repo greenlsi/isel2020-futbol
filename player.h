9fa229fbbb12fc9c45764a100a241fbe850518cb
Jose M. Moya <jose.m.moya@gmail.com>
2020-03-21 14:32:35 +0100

    Update player.h

--- player.h
+++ player.h
@@ -1,18 +1,7 @@
- 
-
-player.h 
-
 #include "fsm.h" 
 
- 
-
-struct player_t; 
-
-typedef struct player_t player_t; 
-
- 
+struct player_t;
+typedef struct player_t player_t;
 
 void player_init (player_t* p, int id); 
-
-fsm_t* fsm_new_player (int id); 
-
+fsm_t* fsm_new_player (int id);
