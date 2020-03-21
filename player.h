cefa8df8550d5fc07f3489f55abb1314a581a8f8
Jose M. Moya <jose.m.moya@gmail.com>
2020-03-21 14:30:09 +0100

    Create player.h

---
+++ player.h
@@ -0,0 +1,18 @@
+ 
+
+player.h 
+
+#include "fsm.h" 
+
+ 
+
+struct player_t; 
+
+typedef struct player_t player_t; 
+
+ 
+
+void player_init (player_t* p, int id); 
+
+fsm_t* fsm_new_player (int id); 
+
